f0e033f43d73a5844a9201551d2cb86427fef664
marcovicar <marcosvictormbi85@gmail.com>
2024-12-08 18:19:39 +0000

    config to S3

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -96,6 +96,21 @@ export default buildConfig({
     //     },
     //   },
     // }),
+    s3Storage({
+      collections: {
+        'media': {
+          prefix: "media",
+        },
+      },
+      bucket: process.env.S3_BUCKET || "",
+      config: {
+        credentials: {
+          accessKeyId: process.env.S3_ACCESS_KEY_ID || "",
+          secretAccessKey: process.env.S3_SECRET_ACCESS_KEY || "",
+        },
+        region: process.env.S3_REGION || "",
+      }
+    }),
     seoPlugin({
       collections: [
         'pages',
